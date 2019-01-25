fdb6dc213b4cdaef437ee1d8aa6daffa88a10007
bradt123 <bradt123@github.com>
2019-01-25 13:51:26 -0400

    filtro gestion certificados trabajo

--- sis_organigrama/vista/certificado_planilla/CertificadoEmetido.php
+++ sis_organigrama/vista/certificado_planilla/CertificadoEmetido.php
@@ -50,9 +50,9 @@ header("content-type: text/javascript; charset=UTF-8");
         },
     cmbGestion : new Ext.form.ComboBox({
         name:'gestion',
-        store:['2018','2017','2016'],
+        store:['2019','2018','2017','2016','2015','2014','2013','2012','2011','2010','2009','2008','2007','2006','2005','2004'],
         typeAhead: true,
-        value: '2018',
+        value: '2019',
         mode: 'local',
         triggerAction: 'all',
         emptyText:'Géstion...',
