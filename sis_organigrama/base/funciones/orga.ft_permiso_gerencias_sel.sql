f45190eda77eb15f617bc56b047bf800992b1c58
Franky <franklin.espinoza.alvarez@gmail.com>
2020-08-27 16:56:12 -0400

    Quitando el parallel

--- sis_organigrama/base/funciones/orga.ft_permiso_gerencias_sel.sql
+++ sis_organigrama/base/funciones/orga.ft_permiso_gerencias_sel.sql
@@ -281,7 +281,6 @@ LANGUAGE 'plpgsql'
 VOLATILE
 CALLED ON NULL INPUT
 SECURITY INVOKER
-PARALLEL UNSAFE
 COST 100;
 
 ALTER FUNCTION orga.ft_permiso_gerencias_sel (p_administrador integer, p_id_usuario integer, p_tabla varchar, p_transaccion varchar)
