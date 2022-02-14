da3b8b51b68b0d5e79cd72d46764613e54272ee2
franklinespinozaalvarez <franklin.espinoza.alvarez@gmail.com>
2022-02-14 10:38:24 -0400

    actualizacion de las fechas de ingreso de los funcionarios

--- sis_organigrama/base/funciones/orga.ft_funcionario_sel.sql
+++ sis_organigrama/base/funciones/orga.ft_funcionario_sel.sql
@@ -6,19 +6,19 @@ CREATE OR REPLACE FUNCTION orga.ft_funcionario_sel (
 )
 RETURNS varchar AS
 $body$
-  /**************************************************************************
-   FUNCION: 		orga.ft_funcionario_sel
-   DESCRIPCIÓN:  listado de funcionario
-   AUTOR: 	    KPLIAN (mzm)
-   FECHA:
-   COMENTARIOS:
-  ***************************************************************************
-   HISTORIA DE MODIFICACIONES:
+/**************************************************************************
+     FUNCION: 		orga.ft_funcionario_sel
+     DESCRIPCIÓN:  listado de funcionario
+     AUTOR: 	    KPLIAN (mzm)
+     FECHA:
+     COMENTARIOS:
+    ***************************************************************************
+     HISTORIA DE MODIFICACIONES:
 
-   DESCRIPCION:
-   AUTOR:
-   FECHA:		21-01-2011
-  ***************************************************************************/
+     DESCRIPCION:
+     AUTOR:
+     FECHA:		21-01-2011
+    ***************************************************************************/
 
 
   DECLARE
@@ -182,7 +182,8 @@ $body$
                           FUNCIO.email_empresa,
                           gecom.f_get_numeros_asignados(''interno'',FUNCIO.id_funcionario) as interno,
                           --''01/03/2021''::date fecha_ingreso,
-                          plani.f_get_fecha_primer_contrato_empleado (tuo.id_uo_funcionario, tuo.id_funcionario, tuo.fecha_asignacion) as fecha_ingreso,
+                          --plani.f_get_fecha_primer_contrato_empleado (tuo.id_uo_funcionario, tuo.id_funcionario, tuo.fecha_asignacion) as fecha_ingreso,
+                          FUNCIO.fecha_ingreso_calculado as fecha_ingreso,
                           PERSON.nombre_completo2 AS desc_person,
                           usu1.cuenta as usr_reg,
                           usu2.cuenta as usr_mod,
@@ -224,13 +225,22 @@ $body$
                           ten.id_especialidad_nivel,
                           ten.nombre as desc_titulo,
 
-                          orga.f_get_funcionario_base_operativa(FUNCIO.id_funcionario) base_operativa
+                          orga.f_get_funcionario_base_operativa(FUNCIO.id_funcionario) base_operativa,
+
+                          (''(''||vcc.codigo_tcc ||'') '' ||vcc.descripcion_tcc)::varchar AS centro_costo,
+                          cp.codigo_categoria categoria,
+                          (date_part(''year'',age(current_date, FUNCIO.fecha_ingreso_calculado))::varchar ||'' años ''||date_part(''month'',age(current_date, FUNCIO.fecha_ingreso_calculado))::varchar||'' meses ''||date_part(''day'',age(current_date, FUNCIO.fecha_ingreso_calculado))::varchar||'' dias'')::varchar tiempo_empresa,
+                          (SELECT afp.tipo_jubilado FROM plani.tfuncionario_afp afp where afp.id_funcionario =  FUNCIO.id_funcionario and afp.fecha_fin is null order by afp.fecha_ini desc limit 1)::varchar jubilado
+
+                          --dep.nombre_unidad
+                          --(select org.nombre_unidad  from orga.tuo org where org.id_uo =orga.f_get_uo_departamento(tuo.id_uo, NULL::integer, NULL::date))::varchar nombre_unidad
 
                           FROM orga.tfuncionario FUNCIO
                           inner join orga.tuo_funcionario tuo on tuo.id_funcionario = FUNCIO.id_funcionario AND
                           tuo.fecha_asignacion  in (select fecha_asignacion
                                                       from tt_orga_filtro where id_funcionario = FUNCIO.id_funcionario)
                           inner join orga.tcargo tca on tca.id_cargo = tuo.id_cargo
+                          inner join orga.ttemporal_cargo ttc on ttc.id_temporal_cargo = tca.id_temporal_cargo
                           inner join orga.toficina tof on tof.id_oficina = tca.id_oficina
                           inner join param.tlugar tlo on tlo.id_lugar = tca.id_lugar
                           INNER JOIN SEGU.vpersona PERSON ON PERSON.id_persona=FUNCIO.id_persona
@@ -243,6 +253,12 @@ $body$
                           left join orga.tespecialidad_nivel ten on ten.id_especialidad_nivel = FUNCIO.id_especialidad_nivel
 
                           '||v_inner||'
+
+                          left join orga.tcargo_presupuesto tcp on tcp.id_cargo = tuo.id_cargo and tcp.id_gestion = (select tg.id_gestion from param.tgestion tg where tg.gestion = date_part(''year'',tuo.fecha_asignacion))
+                          left join param.vcentro_costo vcc on vcc.id_centro_costo = tcp.id_centro_costo
+                          left join pre.tpresupuesto pre on pre.id_centro_costo = tcp.id_centro_costo
+                          left join pre.vcategoria_programatica cp on cp.id_categoria_programatica = pre.id_categoria_prog
+
                           WHERE tuo.estado_reg = ''activo'' and tuo.tipo = ''oficial'' and '||v_filtro||'';
 
 		  else
@@ -300,11 +316,15 @@ $body$
                             ten.id_especialidad_nivel,
                           ten.nombre as desc_titulo,
 
-                          orga.f_get_funcionario_base_operativa(FUNCIO.id_funcionario) base_operativa
-
+                          orga.f_get_funcionario_base_operativa(FUNCIO.id_funcionario) base_operativa,
+                          null::varchar,
+                          null::varchar,
+                          null::varchar,
+						  null::varchar
                           FROM orga.tfuncionario FUNCIO
                           left join orga.tuo_funcionario tuo on tuo.id_funcionario = FUNCIO.id_funcionario
                           left join orga.tcargo tca on tca.id_cargo = tuo.id_cargo
+                          inner join orga.ttemporal_cargo ttc on ttc.id_temporal_cargo = tca.id_temporal_cargo
                           left join orga.toficina tof on tof.id_oficina = tca.id_oficina
                           left join param.tlugar tlo on tlo.id_lugar = tca.id_lugar
                           INNER JOIN SEGU.vpersona PERSON ON PERSON.id_persona=FUNCIO.id_persona
@@ -463,6 +483,7 @@ $body$
                     tuo.fecha_asignacion  in (select fecha_asignacion
                                                 from tt_orga_filtro where id_funcionario = FUNCIO.id_funcionario)
                     inner join orga.tcargo tca on tca.id_cargo = tuo.id_cargo
+                    inner join orga.ttemporal_cargo ttc on ttc.id_temporal_cargo = tca.id_temporal_cargo
                     inner join orga.toficina tof on tof.id_oficina = tca.id_oficina
                     inner join param.tlugar tlo on tlo.id_lugar = tca.id_lugar
                     INNER JOIN SEGU.vpersona PERSON ON PERSON.id_persona=FUNCIO.id_persona
@@ -474,6 +495,14 @@ $body$
                     left join orga.tespecialidad_nivel ten on ten.id_especialidad_nivel = FUNCIO.id_especialidad_nivel
 
                     '||v_inner||'
+
+                    left join orga.tcargo_presupuesto tcp on tcp.id_cargo = tuo.id_cargo and tcp.id_gestion = (select tg.id_gestion from param.tgestion tg where tg.gestion = date_part(''year'',tuo.fecha_asignacion))
+                    left join param.vcentro_costo vcc on vcc.id_centro_costo = tcp.id_centro_costo
+                    left join pre.tpresupuesto pre on pre.id_centro_costo = tcp.id_centro_costo
+                    left join pre.vcategoria_programatica cp on cp.id_categoria_programatica = pre.id_categoria_prog
+                    --left join orga.tuo dep ON dep.id_uo = orga.f_get_uo_departamento(tuo.id_uo, NULL::integer, NULL::date)
+
+
                     WHERE tuo.estado_reg = ''activo'' and tuo.tipo = ''oficial'' and '||v_filtro||'';
         else
       		v_consulta:='SELECT
@@ -481,6 +510,7 @@ $body$
                     FROM orga.tfuncionario FUNCIO
                     left join orga.tuo_funcionario tuo on tuo.id_funcionario = FUNCIO.id_funcionario
                     left join orga.tcargo tca on tca.id_cargo = tuo.id_cargo
+                    inner join orga.ttemporal_cargo ttc on ttc.id_temporal_cargo = tca.id_temporal_cargo
                     left join orga.toficina tof on tof.id_oficina = tca.id_oficina
                     left join param.tlugar tlo on tlo.id_lugar = tca.id_lugar
                     INNER JOIN SEGU.vpersona PERSON ON PERSON.id_persona=FUNCIO.id_persona
@@ -1295,3 +1325,6 @@ VOLATILE
 CALLED ON NULL INPUT
 SECURITY INVOKER
 COST 100;
+
+ALTER FUNCTION orga.ft_funcionario_sel (par_administrador integer, par_id_usuario integer, par_tabla varchar, par_transaccion varchar)
+  OWNER TO postgres;
